5dd1bbbd4e8185152bb79bdfbd6ec5dc88436f81
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-06-03 08:43:22 -0500

    Header and Sidenav cleanup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+console.log("I'm Connected");
 M.AutoInit();
 
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.materialboxed');
 	var instances = M.Materialbox.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, { edge: 'right' });
 });
