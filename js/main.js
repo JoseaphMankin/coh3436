2cf510c558f06126302b4d34d6440efe40f07e87
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-06-03 18:29:35 -0500

    properties rough in

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.sidenav');
 	var instances = M.Sidenav.init(elems, { edge: 'right' });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.slider');
+	var instances = M.Slider.init(elems, options);
+});
