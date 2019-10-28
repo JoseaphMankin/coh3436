08ae950bec82d831def6349f88416b71d88db78c
Joseaph Mankin <JoseaphMankin@gmail.com>
2019-10-28 11:46:51 -0500

    updates to property and photos

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.slider');
 	var instances = M.Slider.init(elems, options);
 });
+
+// EmailJS
+
+var template_params = {
+	reply_to: 'reply_to_value',
+	from_name: 'from_name_value',
+	to_name: 'to_name_value',
+	message_html: 'message_html_value',
+};
+
+var service_id = 'default_service';
+var template_id = 'contact_form';
+emailjs.send(service_id, template_id, template_params);
